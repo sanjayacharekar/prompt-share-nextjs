f88cc02c98d6efa6847154a56ff0a0915fdaabeb
SanjayAcharekar <621998sma@gmail.com>
2023-12-10 19:15:19 +0530

    intial commit

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Form from "@components/Form";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const CreatePrompt = () => {
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const response = await fetch("api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  return (
+    <>
+      <Form
+        type="Create"
+        setPost={setPost}
+        post={post}
+        submitting={submitting}
+        handleSubmit={createPrompt}
+      />
+    </>
+  );
+};
+
+export default CreatePrompt;
